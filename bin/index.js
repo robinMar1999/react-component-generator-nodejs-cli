40164157c8774fb3f932deab894473d585c63da1
robin <kashyap.robin.9277@gmail.com>
2022-01-05 22:04:04 +0530

    generate basic functional component

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,49 @@
 #!/usr/bin/env node
 
-console.log("hello world!!!");
+const fs = require("fs");
+const path = require("path");
 
-console.log(process.argv);
+let currPath = path.join(process.cwd(), "components");
 
-console.log(process.argv.length);
-console.log(process.cwd());
+const length = process.argv.length;
+
+if (length < 3) {
+  console.log("ERROR : Component name is required!!!");
+  process.exit(0);
+}
+
+const NAME = process.argv[2];
+
+// Creating Directory
+if (length > 3) {
+  for (let i = 3; i < length; i++) {
+    currPath = path.join(currPath, process.argv[i]);
+  }
+}
+
+currPath = path.join(currPath, process.argv[2]);
+
+if (!fs.existsSync(currPath)) {
+  fs.mkdirSync(currPath, { recursive: true });
+}
+
+// Creating JS File
+const jstext = `const ${NAME} = () => {
+  return (
+    <div className={classes.${NAME}}>
+      
+    </div>
+  )
+}
+
+export default ${NAME}
+`;
+
+fs.writeFileSync(path.join(currPath, `${NAME}.js`), jstext);
+
+// Creating CSS File
+const csstext = `.${NAME}{
+
+}`;
+
+fs.writeFileSync(path.join(currPath, `${NAME}.module.css`), csstext);
